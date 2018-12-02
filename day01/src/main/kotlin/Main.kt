10060e89ed0555b3c7f80a3e18c2969537beedf2
Pavel Rozenblioum <pavel.rozenblioum@jayway.com>
2018-12-02 11:25:53 -0800

    days 1 and 2

--- day01/src/main/kotlin/Main.kt
+++ day01/src/main/kotlin/Main.kt
@@ -1,7 +1,19 @@
 import java.io.File
 
 fun main() {
-    val input = File(ClassLoader.getSystemResource("input.txt").file)
+    val input = File(ClassLoader.getSystemResource("input.txt").file).readLines()
+    println(input.map { it.toInt() }.sum())
+    println(doStuff(input.map { it.toInt() }, mutableSetOf()))
 
-    // Write solution here!
+}
+
+fun doStuff(values: List<Int>, visited: MutableSet<Int>): Int {
+    var position = 0
+    var sumSofar = 0
+    while (sumSofar !in visited) {
+        visited.add(sumSofar)
+        sumSofar += values[position]
+        position = (position + 1) % values.size
+    }
+    return sumSofar
 }
--- day02/src/main/kotlin/Main.kt
+++ day02/src/main/kotlin/Main.kt
@@ -1,7 +1,33 @@
 import java.io.File
+import java.lang.Math.min
 
 fun main() {
-    val input = File(ClassLoader.getSystemResource("input.txt").file)
+    val strings = File(ClassLoader.getSystemResource("input.txt").file).readLines()
+    strings.map {
+        it.toList()
+            .groupBy { it }
+            .mapValues { it.value.size }.run {
+                (min(1, count { it.value == 2 }) to min(1, count { it.value == 3 }))
+            }
+    }.fold(0 to 0) { (left1, right1), (left2, right2) ->
+        left1 + left2 to right1 + right2
+    }.run {
+        println(first * second)
+    }
 
-    // Write solution here!
+    (0 until strings.size).flatMap { first ->
+        (first + 1 until strings.size).mapNotNull { second ->
+            if (differByOne(strings[first], strings[second])) {
+                (0 until strings[first].length).mapNotNull {
+                    if (strings[first][it] == strings[second][it]) strings[second][it] else null
+                }.joinToString(separator = "")
+            } else null
+        }
+    }.first().run {
+        println(this)
+    }
 }
+
+
+fun differByOne(str1: String, str2: String) =
+    (0 until str1.length).map { if (str1[it] == str2[it]) 0 else 1 }.sum() == 1
